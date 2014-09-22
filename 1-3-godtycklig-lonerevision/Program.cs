1df1cd6c0e569f40e6c7687d709274e215514412
Mathias Lundberg <ml223nw@student.lnu.se>
2014-09-22 15:21:50 +0200

    Added methods

---
+++ 1-3-godtycklig-lonerevision/Program.cs
@@ -0,0 +1,27 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 1-3-godtycklig-lonerevision
+{
+    class Program
+     {
+        static void Main(string[] args)
+        {
+
+        }
+
+        private static void ProcessSalaries(int count)
+
+        {
+
+        }
+
+        private static void ReadInt(string prompt)
+
+        {
+
+        }
