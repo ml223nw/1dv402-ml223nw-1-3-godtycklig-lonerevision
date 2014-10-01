a7ca84205eb9b59b34049e453863b4a61c0898be
Mathias Lundberg <ml223nw@student.lnu.se>
2014-10-01 23:23:56 +0200

    Finished and reported. Pushed to Github later because of issues.

--- 1-3-godtycklig-lonerevision/Program.cs
+++ 1-3-godtycklig-lonerevision/Program.cs
@@ -11,10 +11,30 @@ namespace godtycklig_lonerevision
     {
         static void Main(string[] args)
         {
-            int amountOfSalaries = ReadInt("Ange antal löner att mata in: ");
-            ProcessSalaries(amountOfSalaries);
+            // Beskriver vilken konsol tangent som har tryckts ned.
+            ConsoleKeyInfo quitProgram;
+
+            // Användaren matar in antal löner som hämtas ReadInt metoden.
+            // Tryck någon tangent för omstart av programmet.
+            do
+            {
+                int amountOfSalaries = ReadInt("Ange antal löner att mata in: ");
+                ProcessSalaries(amountOfSalaries);
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.ForegroundColor = ConsoleColor.White;
+                Console.WriteLine("Tryck på någon tangent för ny beräkning - ESC avslutar");
+                quitProgram = Console.ReadKey();
+                Console.ResetColor();
+                Console.Clear();
+
+                //ESC avslutar programmet.
+            } while (quitProgram.Key != ConsoleKey.Escape);
+
+
         }
 
+        // Användaren matar in de löner som som angetts i ReadInt.
+        // Arrayer skapas.
         private static void ProcessSalaries(int count)
         {
             Console.WriteLine("\n");
@@ -25,7 +45,7 @@ namespace godtycklig_lonerevision
                 String prompt = String.Format("Ange lön nummer {0}: ", i + 1);
                 salaries[i] = ReadInt(prompt);
             }
-            //Sortera array
+            // Arrayer sorteras.
             Array.Sort(salaries);
             int median = 0;
             int average = 0;
@@ -34,14 +54,15 @@ namespace godtycklig_lonerevision
             //Jämför om talet är jämt eller ojämt.
             switch (salaries.Length % 2)
             {
-                //Om talet är jämt.
+                // Om talet är jämt.
                 case 0:
                     median = (salaries[salaries.Length / 2] + salaries[salaries.Length / 2 - 1]) / 2;
                     break;
-                //Om talet är ojämt.
+                // Om talet är ojämt.
                 case 1:
                     median = salaries[salaries.Length / 2];
                     break;
+                // Skriver ut alla uträkningar.
             }
             Console.WriteLine("\n-------------------------------");
             Console.WriteLine("Medianlön:\t {0,10:c0}", median);
@@ -56,18 +77,19 @@ namespace godtycklig_lonerevision
             //Skriver ut lönerna i arrayen
             for (int cols = 0; cols < salaries.Length; cols++)
             {
-            Console.Write("{0,6} ", salaries[cols]);
+                Console.Write("{0,6} ", salaries[cols]);
 
-               //När tre punkter har skrivits ut, byt rad
-               if (cols % 3 == 2) //Tex när cols är 2, 5, 8 osv..
-               {
-                   Console.WriteLine();
-               }
+                // Byter rad efter att tre punkter har skrivits ut.
+                if (cols % 3 == 2)
+                {
+                    Console.WriteLine();
+                }
 
             } Console.WriteLine("\n");
 
-         } 
+        }
 
+        // Om användarens inmatning inte är ett heltal i for av nummer så skrivs ett felmeddelande ut.
         private static int ReadInt(string prompt)
         {
             int userInput = 0;
@@ -90,6 +112,7 @@ namespace godtycklig_lonerevision
                 }
             }
 
+            // Användaren får ett nytt försök att mata in.
             return userInput;
         }
 
